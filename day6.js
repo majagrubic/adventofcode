c3cfd61b4f2f789cfd5698f2ba659d51eb9cff3f
Maja Grubic <princessmaja@gmail.com>
2023-12-06 08:56:56 +0100

    Day6 solution

---
+++ day6.js
@@ -0,0 +1,44 @@
+const { parse } = require('path');
+
+const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('input.txt'),
+});
+
+//for the first part
+let sum = 0;
+let times = [];
+let distances = [];
+let time = 0;
+let distance = 0;
+
+lineReader.on('line', function (line) {
+    const parts = line.split(': ');
+    const value = parts[1].trim().split(' ').join('');
+    if (parts[0].includes('Time')) {
+        time = parseInt(value);
+    } else {
+        distance = parseInt(value);
+    }
+});
+
+lineReader.on('close', function () {
+    const waysToWin = [];
+  //for (let i = 0; i < times.length; i++) {
+       let winningTimes = 0;
+       const duration = time;
+       for (let t = 0; t <= duration; t++) {
+           const speed = t;
+           const remainingTime = duration - t;
+           const currentDistance = speed * remainingTime;
+          // console.log(time + ' ' + distance);
+           if (currentDistance > distance) {
+               winningTimes++;
+           }
+       }
+       //console.log('===');
+       waysToWin.push(winningTimes);
+       //console.log(winningTimes);
+ //  }
+   const result = waysToWin.reduce((a, b) => a * b, 1);
+   console.log(result);
+});
