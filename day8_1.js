151d8b6b4fb991c3160f8fbb55f75f08b3aa0757
Maja Grubic <princessmaja@gmail.com>
2023-12-08 13:58:12 +0100

    Solution to day 8

--- day8_1.js
+++ day8_1.js
@@ -1,7 +1,7 @@
 const { parse } = require('path');
 
 const lineReader = require('readline').createInterface({
-    input: require('fs').createReadStream('input.txt'),
+  input: require('fs').createReadStream('input.txt'),
 });
 
 let instructions = '';
@@ -9,31 +9,31 @@ let lineNr = 0;
 let map = new Map();
 
 lineReader.on('line', function (line) {
-    if (lineNr === 0) {
-        instructions = line;
-    } else if (lineNr > 1) {
-        const parts = line.split(' = ');
-        const point = parts[0];
-        const left = parts[1].split(', ')[0].substring(1);
-        const right = parts[1].split(', ')[1].slice(0, -1);
-        map.set(point, { left, right });
-    }
-    lineNr++;
+  if (lineNr === 0) {
+    instructions = line;
+  } else if (lineNr > 1) {
+    const parts = line.split(' = ');
+    const point = parts[0];
+    const left = parts[1].split(', ')[0].substring(1);
+    const right = parts[1].split(', ')[1].slice(0, -1);
+    map.set(point, { left, right });
+  }
+  lineNr++;
 });
 
 lineReader.on('close', function () {
-   let i = 0;
-   const n = instructions.length;
-   let point = 'AAA';
-   while (point !== 'ZZZ') {
-       const directions = map.get(point);
-       const direction = instructions[i % n];
-       if (direction === 'L') {
-           point = directions.left;
-       } else {
-           point = directions.right;
-       }
-       i++;
-   }
-   console.log(i);
+  let i = 0;
+  const n = instructions.length;
+  let point = 'AAA';
+  while (point !== 'ZZZ') {
+    const directions = map.get(point);
+    const direction = instructions[i % n];
+    if (direction === 'L') {
+      point = directions.left;
+    } else {
+      point = directions.right;
+    }
+    i++;
+  }
+  console.log(i);
 });
